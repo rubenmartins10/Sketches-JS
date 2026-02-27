d3de91135daf6bb88a195f2196e8ae6d3369f59a
Rúben Martins <rubendavidsilvamartins@gmail.com>
2026-02-27 01:27:02 +0000

    Final cleanup: remove ffmpeg from git tracking

--- sketch-03.js
+++ sketch-03.js
@@ -1,9 +1,12 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math');
 
 const settings = {
   dimensions: [ 1080, 1080 ],
-  animate: true
+  animate: true,
+  duration: 10,
+  fps: 30
 };
 
 const sketch = ({ context, width, height }) => {
@@ -20,6 +23,28 @@ const sketch = ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
+    for ( let i = 0; i < agents.length; i++) {
+      const agent = agents[i];
+
+      for (let j = i + 1; j < agents.length; j++) {
+        const other = agents[j];
+
+        const dist = agent.pos.getDistance(other.pos);
+
+        if ( dist > 200 ) continue;
+
+        context.lineWidth = math.mapRange(dist, 0, 200, 12, 1);
+
+
+        context.beginPath();
+        context.moveTo(agent.pos.x, agent.pos.y);
+        context.lineTo(other.pos.x, other.pos.y);
+        context.stroke();
+      }
+    }
+
+
+
     agents.forEach(agent => {
       agent.update();
       agent.draw(context);
@@ -35,6 +60,13 @@ class Vector {
     this.x = x;
     this.y = y;
     this.radius = radius;
+  }
+
+  getDistance(v) { //Teorema de pitágoras para as linhas apenas se fazerem nos pontos próximos
+    const dx = this.x - v.x;
+    const dy = this.y - v.y;
+    return Math.sqrt(dx * dx + dy * dy);
+
   }
 
 }
