83137b276cae701193bb84a4aa0bdcecd9563e2e
Rúben Martins <rubendavidsilvamartins@gmail.com>
2026-02-27 00:34:00 +0000

    Animation

--- sketch-03.js
+++ sketch-03.js
@@ -2,7 +2,8 @@ const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true
 };
 
 const sketch = ({ context, width, height }) => {
@@ -20,31 +21,53 @@ const sketch = ({ context, width, height }) => {
     context.fillRect(0, 0, width, height);
 
     agents.forEach(agent => {
+      agent.update();
       agent.draw(context);
+      agent.bounce(width, height);
     });
   };
 };
 
 canvasSketch(sketch, settings);
 
-class Point {
+class Vector {
   constructor(x, y, radius) {
     this.x = x;
     this.y = y;
     this.radius = radius;
   }
+
 }
 
 class Agent {
   constructor(x, y) {
-    this.pos = new Point(x, y);
-    this.radius = 10;
+    this.pos = new Vector(x, y);
+    this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
+    this.radius = random.range(4, 12);
   }
+
+  bounce(width, height) {
+    if (this.pos.x <= 0 || this.pos.x >= width) this.vel.x *= -1;
+    if (this.pos.y <= 0 || this.pos.y >= height) this.vel.y *= -1;
+  }
+
+  update(){
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+  }
+
   draw(context) {
-    context.fillStyle = 'black';
+
+    context.save();
+    context.translate(this.pos.x, this.pos.y);
+
+    context.lineWidth = 3;
 
     context.beginPath();
-    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
+    context.arc(0, 0, this.radius, 0, Math.PI * 2);
     context.fill();
+    context.stroke(); 
+
+    context.restore();
   }
 }
